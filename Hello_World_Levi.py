b203d51369254e3bd8760aa9a285878f85d1ba86
LeviZm <levimelangton@gmail.com>
2026-01-15 19:01:34 -0500

    Add Module 1 Collaboration repo and Hello_World_name.py file.

---
+++ Hello_World_Levi.py
@@ -0,0 +1,9 @@
+# Hello_World_Levi
+import string
+
+name = "Levi"
+print("Hello, World! My name is " + name + ".")
+
+''' The different immutable types in Python include integers, floats, strings, booleans, tuples, and bytes.'''
+
+''' The different mutable types in Python include lists, dictionaries, sets, and bytearrays.'''
