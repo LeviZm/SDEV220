14a4fe1f75e4c124643d9eeef8149d107e80eab4
LeviZm <levimelangton@gmail.com>
2026-02-13 14:40:00 -0500

    Add header to Module 3 Lab - Case Study.py

--- Module 3/Module 3 Lab - Case Study.py
+++ Module 3/Module 3 Lab - Case Study.py
@@ -1,4 +1,17 @@
-# Module 3 Lab - Case Study: Functions and Classes
+''' Module 3 Lab - Case Study: Functions and Classes
+    Levi Melangton
+    Module 3 Lab - Case Study.py
+    This program defines a Vehicle superclass and an Automobile class. The Automobile class inherits from the Vehicle class and includes
+    additional attributes specific to automobiles. The program collects user input to create an instance of the Automobile class and
+    displays the information about the automobile.
+
+    v_type = vehicle type
+    year = year of the automobile
+    make = make of the automobile
+    model = model of the automobile
+    doors = number of doors (2 or 4)
+    roof = roof type (solid or sun roof)
+'''
 
 class Vehicle:
     def __init__(self,type):
